c72c01bf01769716a197a7fc0e6637edab0fdfd0
MYZT3RY <d1maz.d@yandex.ru>
2021-11-11 03:00:30 +0300

    Class for variables

---
+++ cookieClicker/variables.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cookieClicker {
+    class variables { // class with variables
+        private long cookies; // variable for cookies
+
+        public variables(long cookies) { // constructor
+            this.cookies = cookies;
+        }
+
+        public void setCookies(long cookies) { // set value for cookies
+            this.cookies = cookies;
+        }
+
+        public long getCookies() { // get value of cookies
+            return cookies;
+        }
+    }
+}
